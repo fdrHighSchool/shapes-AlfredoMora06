64266159f4f75bb0f7b8b1f5ee8f2f52fb9b69c5
AlfredoMora06 <agmmendez123@gmail.com>
2020-10-13 12:05:08 -0400

    added choices

--- Shapes.java
+++ Shapes.java
@@ -1,25 +1,41 @@
 import java.util.*;
 
-public class Shapes{
+public class Shapes {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    System.out.print("Enter the side length: ");
-    double side = input.nextDouble();
-    square(side);
+    System.out.print("Enter 1 for rectangle calculation and 2 for square: ");
+    int choice = input.nextInt();
+    if (choice == 1) {
+      System.out.print("Enter the length: ");
+      double length = input.nextDouble();
+      System.out.print("Enter the width: ");
+      double width = input.nextDouble();
+      rectangle(length, width);
+    } else {
+      System.out.print("Enter the side: ");
+      double side = input.nextDouble();
+      square(side);
+    }
+
   }
 
   /*
-   * N: square
-   * P: to calculate the area of any square
-   * I: a side measure (double)
+   * N: square P: to calculate the area of any square I: a side measure (double)
    * R:
    */
-  public static void square(double s){
-    //side times side
+  public static void square(double s) {
+    // side times side
     double a = s * s;
 
-    //generate some output
+    // generate some output
     System.out.println("A square with side " + s + " has an area of " + a);
-  }//close square method
+  }// close square method
 
+  public static void rectangle(double x, double y) {
+    // side times side
+    double a = x * y;
+
+    // generate some output
+    System.out.println("A rectangle with length " + x + " and width " + y + " has an area of " + a);
+  }// close rectangle method
 }
